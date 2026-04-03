734dd7d60637ff76367ba9f9a3633b40a9c3e037
Kimtanhihiii <shyn13052004@gmail.com>
2026-04-03 13:41:51 +0700

    Configure mongoose online to use shopping_online database

--- server/utils/MongooseUtil.js
+++ server/utils/MongooseUtil.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose');
 const MyConstants = require('./MyConstants');
 
 const uri = process.env.MONGODB_URI || MyConstants.MONGODB_URI;
+const dbName = process.env.DB_DATABASE || MyConstants.DB_DATABASE;
 
-mongoose.connect(uri)
+mongoose.connect(uri, { dbName })
   .then(() => {
-    console.log(`Connected MongoDB: ${uri}`);
+    console.log(`Connected MongoDB database: ${dbName}`);
   })
   .catch((err) => console.error(err));
